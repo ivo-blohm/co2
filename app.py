1e15d9309d16001962ba07d0e9aee51207b61e56
ivo <47453671+ivo-blohm@users.noreply.github.com>
2025-01-13 13:27:19 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import pickle
 from sklearn.tree import DecisionTreeRegressor
 
 def co2_mean():
-    data = pd.read_csv("co2_data_merged.csv")
+    data = pd.read_csv("co2_data_merged_eng.csv")
     mean=data["CO2"].mean()
     return mean
 
 def load_data():
-    data = pd.read_csv("co2_data_merged.csv")
+    data = pd.read_csv("co2_data_merged_eng.csv")
     del(data["CO2"])
-    return data.sort_values("Postleitzahl",ascending=True)
+    return data.sort_values("Zipcode",ascending=True)
 
 def load_model():
     file_path = "tree_model.sav"
@@ -30,24 +30,24 @@ st.header("Predict CO2 Emissions")
 col1_1, col1_2, col1_3, col1_4 = st.columns([1,1,1,1])
 col2_1, col2_2, col2_3, col2_4 = st.columns([1,1,1,1])
 
-nutzung = col1_1.selectbox("Nutzung", data["Nutzung"].unique())    
-energieträger = col1_2.selectbox("Energieträger", data["Energieträger"].unique())   
-kanton = col1_3.selectbox("Kanton", data["Kanton"].unique())   
-plz = col1_4.selectbox("Postleitzahl", data["Postleitzahl"].unique())    
+nutzung = col1_1.selectbox("Utilization", data["Utilization"].unique())    
+energieträger = col1_2.selectbox("Energy Source", data["Energy_Source"].unique())   
+kanton = col1_3.selectbox("Canton", data["Canton"].unique())   
+plz = col1_4.selectbox("Zipcode", data["Zipcode"].unique())    
 
-ebf = col2_1.slider("EBF", data["EBF"].min(), data["EBF"].max())
-jahr = col2_2.slider("Jahr", data["Jahr"].min(), data["Jahr"].max())
-baujahr = col2_3.slider("Baujahr", data["Baujahr"].min(), data["Baujahr"].max())
-erneuerbar = col2_4.slider("% Erneuerbarer Energie", data["Anteil_Erneuerbare_Energie"].min(), data["Anteil_Erneuerbare_Energie"].max())
+ebf = col2_1.slider("Heated Area (m²)", data["Heated_Area"].min(), data["Heated_Area"].max())
+jahr = col2_2.slider("Year of Measurement", data["Year_of_Measurement"].min(), data["Year_of_Measurement"].max())
+baujahr = col2_3.slider("Year of Construction", data["Year_of_Construction"].min(), data["Year_of_Construction"].max())
+erneuerbar = col2_4.slider("Renewable Energy (%)", data["Percentage_Renewable_Energy"].min(), data["Percentage_Renewable_Energy"].max())
 
-last=pd.DataFrame.from_dict({"Nutzung":[nutzung],
-                             "Energieträger": [energieträger],
-                             "Kanton":[kanton],
-                             "Postleitzahl":[plz],
-                             "EBF":[ebf],
-                             "Jahr":[jahr],
-                             "Baujahr":[baujahr],
-                             "Anteil_Erneuerbare_Energie":[erneuerbar]
+last=pd.DataFrame.from_dict({"Utilization":[nutzung],
+                             "Energy_Source": [energieträger],
+                             "Canton":[kanton],
+                             "Zipcode":[plz],
+                             "Heated_Area":[ebf],
+                             "Year_of_Measurement":[jahr],
+                             "Year_of_Construction":[baujahr],
+                             "Percentage_Renewable_Energy":[erneuerbar]
                              })
 data = pd.concat([data, last], axis=0)
 data = pd.get_dummies(data)
